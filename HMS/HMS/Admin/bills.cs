ca216c3b4379030d0796bb04ff5c6beab69fd833
Githuzaifa <huzaifamajeed56@gmail.com>
2023-11-29 10:26:12 +0500

    Create form for admin to view bill reports
    This commit adds feature for the admin to view bill reports

---
+++ HMS/HMS/Admin/bills.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS.Admin
+{
+    public partial class bills : Form
+    {
+        public bills()
+        {
+            InitializeComponent();
+        }
+        static List<bill> bill = new List<bill>();
+
+        private void bills_Load(object sender, EventArgs e)
+        {
+            bill = crud.getAll<bill>("/bill/bills");
+            
+            
+
+            dataGridView1.DataSource = bill;
+            dataGridView1.Columns.RemoveAt(0);
+        }
+    }
+}
