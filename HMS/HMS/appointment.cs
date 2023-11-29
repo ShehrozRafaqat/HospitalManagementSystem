3cbb301b36738eec68a50402ec3506803b1c8c4e
Githuzaifa <huzaifamajeed56@gmail.com>
2023-11-29 10:31:46 +0500

    Create appointment class
    This commit adds a class named appointment to the project which contains type, time, doctor, and patient related to the appointment.

---
+++ HMS/HMS/appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS
+{
+    public class appointment
+    {
+        public appointment(string type, DateTime date_time,string doc,string patient)
+        {
+            this.type = type;
+            this.date_time = date_time;
+            this.doc = doc;
+            this.patient = patient;
+        }
+
+        public string _Id { get; set; }
+        public string doc { get; set; }
+        public string patient { get; set; }
+        public string type { get; set; }
+        public DateTime date_time { get; set; }
+    }
+}
