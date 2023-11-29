a4b62ab0624705728aad59029cb729bce7dd418b
Githuzaifa <huzaifamajeed56@gmail.com>
2023-11-29 10:37:39 +0500

    Create User class
    This commit adds a User class to the project which contains role, id, username, and password of the user.

--- HMS/HMS/User.cs
+++ HMS/HMS/User.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace HMS
 {
-    class User
+    public class User
     {
-        public int Role { get; set; }
+        public string _Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public int Role { get; set; }
 
     }
 }
