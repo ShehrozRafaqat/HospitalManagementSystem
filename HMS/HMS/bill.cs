dde55d153fb7972872bc77e0c9d79aa438355e2c
Githuzaifa <huzaifamajeed56@gmail.com>
2023-11-29 10:33:28 +0500

    Create class named bill
    This commit adds a class to the project named bill that contains amount and the appointment it is related to.

---
+++ HMS/HMS/bill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS
+{
+    class bill
+    {
+        public bill(string appointment,string amount)
+        {
+            this.appointment = appointment;
+            this.amount = amount;
+        }
+
+        public string _Id { get; set; }
+        public string appointment { get; set; }
+        public string amount { get; set; }
+    }
+}
