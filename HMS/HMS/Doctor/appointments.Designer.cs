7b1986440ad85c3edaa5da384f593c0c5a1cf80e
Githuzaifa <huzaifamajeed56@gmail.com>
2023-11-29 10:27:49 +0500

    Create form to view all appointments
    This commit adds a feature for the doctor to view all of his appointments.

---
+++ HMS/HMS/Doctor/appointments.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace HMS.Doctor
+{
+    partial class appointments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.appointmentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.billsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.appointmentsToolStripMenuItem,
+            this.billsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // appointmentsToolStripMenuItem
+            // 
+            this.appointmentsToolStripMenuItem.Name = "appointmentsToolStripMenuItem";
+            this.appointmentsToolStripMenuItem.Size = new System.Drawing.Size(95, 20);
+            this.appointmentsToolStripMenuItem.Text = "Appointments";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 426);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // billsToolStripMenuItem
+            // 
+            this.billsToolStripMenuItem.Name = "billsToolStripMenuItem";
+            this.billsToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.billsToolStripMenuItem.Text = "Bills";
+            this.billsToolStripMenuItem.Click += new System.EventHandler(this.billsToolStripMenuItem_Click);
+            // 
+            // appointments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "appointments";
+            this.Text = "appointments";
+            this.Load += new System.EventHandler(this.appointments_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem appointmentsToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem billsToolStripMenuItem;
+    }
+}
---
+++ HMS/HMS/Doctor/appointments.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS.Doctor
+{
+    public partial class appointments : Form
+    {
+        public static List<appointment> Appointments = new List<appointment>();
+        static List<appointment> docApp = new List<appointment>();
+        public appointments()
+        {
+            InitializeComponent();
+        }
+
+        private void appointments_Load(object sender, EventArgs e)
+        {
+            Appointments = crud.getAll<appointment>("/appointment/appointments");
+            foreach (appointment app in Appointments)
+            {
+                if (app.doc == Login_Page.user_id)
+                    docApp.Add(app);
+            }
+            dataGridView1.DataSource = docApp;
+            dataGridView1.Columns.RemoveAt(0);
+            // Create a Button Column
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.HeaderText = "Delete";
+            buttonColumn.Text = "Delete";
+            buttonColumn.UseColumnTextForButtonValue = true;
+
+            // Add the Button Column to the DataGridView
+            dataGridView1.Columns.Add(buttonColumn);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns[3].Index && e.RowIndex >= 0)
+            {
+                crud.delete("/appointment/delete", docApp[e.RowIndex]._Id);
+                docApp.RemoveAt(e.RowIndex);
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void billsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ViewBill b = new ViewBill();
+            b.Show();
+        }
+    }
+}
